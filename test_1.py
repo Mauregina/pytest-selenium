2887c33f67eeb178fa03aa5a4c46138c80df6ecb
Maura Regina <maura.costa@indt.org.br>
2022-02-15 18:44:07 -0400

    implementa teste automatico efetua login sem informar user senha
    Signed-off-by: Maura Regina <maura.costa@indt.org.br>

--- test_1.py
+++ test_1.py
@@ -1,18 +1,21 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 URL = 'https://www.saucedemo.com/'
 
-driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+class Test1:
+    @pytest.fixture()
+    def setup(self):
+        self.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        self.driver.get(URL)
+        yield #o que vem depois do yield é executado apenas no final do teste
+        self.driver.quit()
 
-driver.get(URL)
-driver.find_element(By.ID, 'login-button').click()
+    def test_click_login_button(self, setup):
+        self.driver.find_element(By.ID, 'login-button').click()
+        assert self.driver.current_url == URL, 'Página requerida não encontrada!'
+        error_msg = self.driver.find_element(By.CLASS_NAME, 'error-message-container').text
+        assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada!'
 
-assert driver.current_url == URL, 'Página requerida não encontrada!'
-
-error_msg = driver.find_element(By.CLASS_NAME, 'error-message-container').text
-
-assert error_msg == 'Epic sadface: Username is required', 'Mensagem de erro não encontrada!'
-
-# driver.quit()
